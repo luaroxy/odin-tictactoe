55a0b6dc64fe78522eb6c40e5795dc7b82f7bcad
luaroxy <luannamsiqueira@gmail.com>
2022-06-05 20:08:14 -0600

    Add x when click

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const game = (() => {
-    const gameboard = (xxx => {
-        let gameboard = [];
-    })();
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+    
+    const updateGameboard = (x) => {
+        gameboard[x]="x";
+        return gameboard;
+    }
 
-    const player = xxx => {
-
+    const displayGameboard = () => {
+        const cellDivs = document.getElementsByClassName('cell');
+        for(let i = 0; i < cellDivs.length; i++) {
+            cellDivs[i].innerText = gameboard[i];
+          };
     };
 
-    return gameboard, player;
+    return {updateGameboard, displayGameboard};
 })();
+
+const Player = () => {
+
+};
+
+function game(e){
+    console.log("deu certo");
+    x=e.dataset.array;
+    Gameboard.updateGameboard(x);
+    Gameboard.displayGameboard();
+    console.log(Gameboard.updateGameboard(x));
+}
